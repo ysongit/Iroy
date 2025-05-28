e6f58a0b518e505be30c054512c5c08ccbce300a
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2025-05-28 16:38:02 -0400

    add Web3Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
-import { Buffer } from 'buffer';
-window.Buffer = Buffer;
-
-import { getDefaultConfig, TomoEVMKitProvider } from '@tomo-inc/tomo-evm-kit';
-import { WagmiProvider } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { TomoEVMKitProvider } from '@tomo-inc/tomo-evm-kit';
 import '@tomo-inc/tomo-evm-kit/styles.css';
 
 import ConnectButton from './components/ConnectButton';
+import { Web3Providers } from './lib/providers';
 
 function App() {
-  const config = getDefaultConfig({
-    clientId: '', // Replace with your clientId
-    appName: 'My TomoEVMKit App',
-    projectId: '', // Note: Every dApp that relies on WalletConnect now needs to obtain a projectId from WalletConnect Cloud.
-    chains: [mainnet, polygon, optimism, arbitrum, base],
-    ssr: false, // If your dApp uses server-side rendering (SSR)
-  });
-
-  const queryClient = new QueryClient();
-
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <TomoEVMKitProvider>
-          {/* Your App */}
-          <h1>Test</h1>
+    <Web3Providers>
+      <TomoEVMKitProvider>
+        <div>
+          <h1>Iroy</h1>
           <ConnectButton />
-        </TomoEVMKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+        </div>
+      </TomoEVMKitProvider>
+    </Web3Providers>
   )
 }
 
-export default App
+export default App;
